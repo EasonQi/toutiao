9e87d4475b113f8b95fdc98c1d0e284e6d271964
easonQi <736352444@qq.com>
2019-08-19 19:48:34 +0800

    first

---
+++ src/App.vue
@@ -0,0 +1,16 @@
+<template>
+  <div id="app">
+   <element-a></element-a>
+  </div>
+</template>
+
+<script>
+import element from './components/element.vue'
+export default {
+  components: {
+    'element-a': element
+  }
+}
+</script>
+<style>
+</style>
---
+++ src/components/element.vue
@@ -0,0 +1,79 @@
+<template>
+<div>
+  <div class="block">
+    <span class="demonstration">默认</span>
+    <el-slider v-model="value1"></el-slider>
+  </div>
+  <div class="block">
+    <span class="demonstration">自定义初始值</span>
+    <el-slider v-model="value2"></el-slider>
+  </div>
+  <div class="block">
+    <span class="demonstration">隐藏 Tooltip</span>
+    <el-slider v-model="value3" :show-tooltip="false"></el-slider>
+  </div>
+  <div class="block">
+    <span class="demonstration">格式化 Tooltip</span>
+    <el-slider v-model="value4" :format-tooltip="formatTooltip"></el-slider>
+  </div>
+  <div class="block">
+    <span class="demonstration">禁用</span>
+    <el-slider v-model="value5" disabled></el-slider>
+  </div>
+  <el-badge :value="12" class="item">
+  <el-button size="small">评论</el-button>
+</el-badge>
+<el-badge :value="3" class="item">
+  <el-button size="small">回复</el-button>
+</el-badge>
+<el-badge :value="1" class="item" type="primary">
+  <el-button size="small">评论</el-button>
+</el-badge>
+<el-badge :value="2" class="item" type="warning">
+  <el-button size="small">回复</el-button>
+</el-badge>
+
+<el-dropdown trigger="click">
+  <span class="el-dropdown-link">
+    点我查看<i class="el-icon-caret-bottom el-icon--right"></i>
+  </span>
+  <el-dropdown-menu slot="dropdown">
+    <el-dropdown-item class="clearfix">
+      评论
+      <el-badge class="mark" :value="12" />
+    </el-dropdown-item>
+    <el-dropdown-item class="clearfix">
+      回复
+      <el-badge class="mark" :value="3" />
+    </el-dropdown-item>
+  </el-dropdown-menu>
+</el-dropdown>
+<el-color-picker v-model="color" show-alpha></el-color-picker>
+  </div>
+</template>
+
+<script>
+export default {
+  data () {
+    return {
+      value1: 0,
+      value2: 50,
+      value3: 36,
+      value4: 48,
+      value5: 42,
+      color: 'rgba(19, 206, 102, 0.8)'
+    }
+  },
+  methods: {
+    formatTooltip (val) {
+      return val / 100
+    }
+  }
+}
+</script>
+<style>
+.item {
+  margin-top: 10px;
+  margin-right: 40px;
+}
+</style>
